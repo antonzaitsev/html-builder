b29685b9ceb4afd3482fd4d5a0e56162692e1d4c
antonzaitsev <3185227@mail.ru>
2021-02-11 11:17:06 +0300

    Up

--- Examples/submit_form.php
+++ Examples/submit_form.php
@@ -8,6 +8,7 @@ $form->addAttribute('name', 'my_best_form');
 $form->addAttribute('id', 'form_id');
 
 $form->addInput('text', 'login', 'myBestLogin');
+$form->addInput('text', 'email', '123@examle.com');
 $form->addHiddenInput('csrf', 'csrfhashstring');
 $form->addHiddenInput('password', 'pa$$word');
 $form->addSubmitInput('Push me');
